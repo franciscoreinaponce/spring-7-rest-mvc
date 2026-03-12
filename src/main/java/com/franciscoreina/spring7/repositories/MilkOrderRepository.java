a6d402e1672bb65a4b5c6fd78ad7f4c92d7f1eeb
Francisco Reina <franciscoreinaponce@gmail.com>
2026-03-12 12:42:31 +0000

    Fix milk order repository

--- src/main/java/com/franciscoreina/spring7/repositories/MilkOrderRepository.java
+++ src/main/java/com/franciscoreina/spring7/repositories/MilkOrderRepository.java
@@ -3,7 +3,7 @@ package com.franciscoreina.spring7.repositories;
 import com.franciscoreina.spring7.domain.MilkOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.rmi.server.UID;
+import java.util.UUID;
 
-public interface MilkOrderRepository extends JpaRepository<MilkOrder, UID> {
+public interface MilkOrderRepository extends JpaRepository<MilkOrder, UUID> {
 }
